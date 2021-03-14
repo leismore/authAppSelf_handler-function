93708367813fbb05eb007885f849773808394d0d
Kyle Chine <kyle.chine@leismore.co>
2021-03-14 14:40:20 +1100

    src done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { generator, GeneratorHostApp, GeneratorErrors, ExpressHandler } from './lib/generator';
+export {
+    generator        as authAppSelf_handler_generator,
+    GeneratorHostApp as authAppSelf_handler_generator_HostApp,
+    GeneratorErrors  as authAppSelf_handler_generator_Errors,
+    ExpressHandler   as ExpressRoutingHandler
+} from './lib/generator';
